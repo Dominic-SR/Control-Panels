e15f886e825baf8deaa876bd42380bdb56967c45
dominic <dominicseduraja3@gmail.com>
2023-03-30 22:41:25 +0530

    side bar work

--- src/Components/sidebar.js
+++ src/Components/sidebar.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import './sidebar.css'
 
 function sidebar() {
   return (
     <div className='side-menu'>
         <div className='brand-name'>
-            Brand...
+            <h1>Brand</h1>
         </div>
         <ul>
             <li><span>Dashboard</span></li>
