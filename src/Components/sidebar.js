b10dcbe5da7a99fe17c037ddbd20f7e6f32dcde6
dominic <dominicseduraja3@gmail.com>
2023-03-31 23:54:17 +0530

    sidebar changes

--- src/Components/sidebar.js
+++ src/Components/sidebar.js
@@ -5,17 +5,17 @@ function sidebar() {
   return (
     <div className='side-menu'>
         <div className='brand-name'>
-            <img src={process.env.PUBLIC_URL + "/logo.png"} height="100"/>
+            {/* <img src={process.env.PUBLIC_URL + "/logo.png"} height="100"/> */}
             <h1>Brand</h1>
         </div>
         <ul>
-            <li><span>Dashboard</span></li>
-            <li><span>Students</span></li>
-            <li><span>Teachers</span></li>
-            <li><span>Schools</span></li>
-            <li><span>Income</span></li>
-            <li><span>Help</span></li>
-            <li><span>Settings</span></li>
+        <li>
+            <i class="fas fa-tachometer-alt"></i><span>Dashboard</span></li>
+            <li><i class="fas fa-file-alt"></i> <span>Forms</span></li>
+            <li><i class="fas fa-table"></i><span>Tables</span></li>
+            <li><i class="fas fa-table"></i><span>Pages</span></li>
+            <li><i class="fas fa-map-marker-alt"></i><span>Map</span></li>
+            <li><i class="fas fa-life-ring"></i><span>Utilities</span></li>
         </ul>
     </div>
   )
